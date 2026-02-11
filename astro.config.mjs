e8227584bb78f5d81bf2f25af335d7199b0a16a7
George Leon <george@scalebloom.com>
2026-02-10 21:12:57 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import criticalCss from "astro-critical-css";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.acornpals.com",
-  integrations: [sitemap(), criticalCss(), mdx()],
+  integrations: [sitemap(), mdx()],
   image: {
     responsiveStyles: true,
-    layout: 'constrained',
+    layout: "constrained",
   },
 });
